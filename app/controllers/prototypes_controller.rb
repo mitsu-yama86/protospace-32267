5f45828b1ebb657c9d5f258dc69c1bdbad97e6aa
mitsu-yama86 <y33ym22m@yahoo.co.jp>
2020-12-17 12:05:34 +0900

    修正#2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
